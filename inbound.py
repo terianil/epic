9716c0c455f7e146510d90b939d50acba9727e7a
Kamil Maciejczek <maciejczek.kamil@gmail.com>
2015-01-07 12:00:34 -0800

    Added inbound POST action

--- inbound.py
+++ inbound.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import *
+import logging
+ 
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/inbound", methods=['POST'])
 def hello():
-	return "hello world, heyeyeye!!!"
+	app.logger.debug('I\'m working.')
+
+	return request.data 
 
 if __name__ == "__main__":
+	app.logger.addHandler(logging.StreamHandler())
+	app.logger.setLevel(logging.DEBUG)
+
 	app.run()
