75210c79aeb00e6a3f5213f3b867e0e36e77bc5d
Kamil Maciejczek <maciejczek.kamil@gmail.com>
2015-01-21 15:57:04 -0800

    Added Window Size based data management tests for QueryProcessor

--- epic/Query.py
+++ epic/Query.py
@@ -1,16 +1,18 @@
-from ConditionType import *
-from AggregationFunctionType import *
+from epic.AggregationFunctionType import AggregationFunctionType
+from epic.ConditionType import ConditionType
+
 
 class Query:
-	functionType = None
-	time = None
-	field = None
-	conditionType = None
-	conditionArgument = None
+    functionType = None
+    time = None
+    field = None
+    conditionType = None
+    conditionArgument = None
 
-	def __init__(self, field="field1", functionType=AggregationFunctionType.Sum, time=5, conditionType=ConditionType.GreaterThan, conditionArgument=100):
-		self.field = field
-		self.functionType = functionType
-		self.time = time
-		self.conditionType = conditionType
-		self.conditionArgument = conditionArgument
+    def __init__(self, field="field1", functionType=AggregationFunctionType.Sum, time=5,
+                 conditionType=ConditionType.GreaterThan, conditionArgument=100):
+        self.field = field
+        self.functionType = functionType
+        self.time = time
+        self.conditionType = conditionType
+        self.conditionArgument = conditionArgument
--- epicTests/QueryProcessorTest.py
+++ epicTests/QueryProcessorTest.py
@@ -1,14 +1,55 @@
-from unittest import TestCase
+import json
+from time import sleep
+from unittest import TestCase, main
+from epic.AggregationFunctionType import AggregationFunctionType
+from epic.ConditionType import ConditionType
+from epic.Query import Query
+from epic.QueryProcessor import QueryProcessor
 
-class QueryProcessorTest(TestCase):
-    def QueryProcessorShouldStoreRightValuesFromInputJson:
-        self.fail()
+__author__ = 'terianil'
 
-    def test_executeQuery(self):
-        self.fail()
 
-    def test_insertNewTuple(self):
-        self.fail()
+class TestQueryProcessor(TestCase):
+    def test_QueryProcessorShouldStoreRightValuesFromInputJson(self):
+        #Arrange
+        query = Query("b", AggregationFunctionType.Avg, 5, ConditionType.GreaterThan, 5)
+        queryProcessor = QueryProcessor(query)
 
-    def test_getQueryData(self):
-        self.fail()
+        tuple1 = json.loads('{"a": 1, "b": 2, "c": 3}')
+        tuple2 = json.loads('{"a": 4, "b": 5, "c": 6}')
+        tuple3 = json.loads('{"a": 7, "b": 8, "c": 9}')
+
+        #Act
+        queryProcessor.insertNewTuple(tuple1)
+        queryProcessor.insertNewTuple(tuple2)
+        queryProcessor.insertNewTuple(tuple3)
+
+        queryData = queryProcessor.getQueryData()
+
+        #Assert
+        self.assertEquals(queryData[0].data, 2)
+        self.assertEquals(queryData[1].data, 5)
+        self.assertEquals(queryData[2].data, 8)
+
+    def test_QueryProcessorShouldRemoveDataOlderThanWindowSize(self):
+        #Arrange
+        query = Query("b", AggregationFunctionType.Avg, 1, ConditionType.GreaterThan, 5)
+        queryProcessor = QueryProcessor(query)
+
+        tuple1 = json.loads('{"a": 1, "b": 2, "c": 3}')
+        tuple2 = json.loads('{"a": 4, "b": 5, "c": 6}')
+
+        #Act
+        queryProcessor.insertNewTuple(tuple1)
+        queryProcessor.insertNewTuple(tuple2)
+
+        sleep(2)
+
+        queryData = queryProcessor.getQueryData()
+
+        #Assert
+        self.assertEquals(len(queryData), 0)
+
+
+if __name__ == '__main__':
+    main()
