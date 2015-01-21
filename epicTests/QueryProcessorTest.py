9a8c6d001ab8223a5ee8cc734a3b886db148d523
Kamil Maciejczek <maciejczek.kamil@gmail.com>
2015-01-21 14:26:26 -0800

    Added QueryProcessorTest

---
+++ epicTests/QueryProcessorTest.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+class QueryProcessorTest(TestCase):
+    def QueryProcessorShouldStoreRightValuesFromInputJson:
+        self.fail()
+
+    def test_executeQuery(self):
+        self.fail()
+
+    def test_insertNewTuple(self):
+        self.fail()
+
+    def test_getQueryData(self):
+        self.fail()
